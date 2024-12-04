ca7711aeddc23928212797ea316531e278ab7c0f
ichksk <good.ksuke.001@gmail.com>
2024-12-05 00:35:22 +0900

    font移動

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
