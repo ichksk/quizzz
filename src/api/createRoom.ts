9dcecc54b3e9230931a737b07fc158d68aaae79b
ichksk <good.ksuke.001@gmail.com>
2024-12-07 09:18:50 +0900

    fix some

--- src/api/createRoom.ts
+++ src/api/createRoom.ts
@@ -3,7 +3,7 @@
 import { apiUrl } from "@/constants/url"
 
 export const createRoom = async () => {
-  const res = await fetch(`${apiUrl}/`)
+  const res = await fetch(`${apiUrl}/rooms`)
   const json = await res.json()
   console.log(json)
 }
